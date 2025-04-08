33d11c7e2319cfcdda3cf28b9f66d9ccf9374bc0
ChrisM679 <cmunson@student.neumont.edu>
2025-04-08 12:41:53 -0600

    Containers Assignments

--- Containers/Containers.cpp
+++ Containers/Containers.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main()
 {
 	//array
-	array<int, 5> arr = {1,2,3,4,5};
+	array<string, 7> arr = {"Monday","tusday","Wensday","Thursday","Friday","Saturday","Sunday"};
 	cout << arr.size() << endl;
 
 	for (int i = 0; i < arr.size(); i++)
@@ -18,31 +18,43 @@ int main()
 		cout << arr[i] << endl;
 	}
 
-	cout << (&arr[0]) << endl;
-	cout << (&arr[1]) << endl;
-	cout << (&arr[2]) << endl;
-	cout << (&arr[3]) << endl;
-	cout << (&arr[4]) << endl;
+	cout << endl;
 
 	//vector
 	vector<int> vec = { 1,2,3,4,5 };
 	vec.push_back(6);
+	vec.push_back(7);
+	vec.pop_back();
 
-	cout << vec.size() << endl;
-	cout << vec.capacity() << endl;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << endl;
+	}
+
+	cout << endl;
 
 	//list
-	list<int> lst = { 1,2,3,4,5 };
-	lst.push_back(6);
-	lst.insert(lst.begin(), 0);
+	list<string> lst = {"Apple", "Orange", "Grape"};
+	lst.insert(lst.begin(), "Watermellon");
+	lst.insert(lst.end(), "Bananas");
+	lst.remove("Grape");
+
+	for (list<string>::iterator it = lst.begin(); it != lst.end(); ++it) {
+		cout << *it << " ";
+	}
+
+	cout << endl;
 
 	//map
 	map<string, int> m;
-	m["riley"] = 46;
-	m["garrett"] = 86;
-	m["mike"] = 100;
+	m["apples"] = 46;
+	m["bananas"] = 86;
+	m["Orange"] = 100;
+	m["bananas"] = 98;
 
-	cout << m["riley"] << endl;
+	for (const auto& fruit : lst) {
+		cout << fruit << " ";
+	}
 
 	return 0;
 }
